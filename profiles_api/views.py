3342cfd30b93c34519665ec98cd09fb792928d44
yoel <rjuanyoel@gmail.com>
2021-10-25 12:50:47 -0400

    archivos fuera del primer commit

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,11 +1,14 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
+from profiles_api import serializers
 
 # Create your views here.
 
 
 class HelloApiView(APIView):
     """ Api View de Prueba """
+    serializer_class = serializers.HelloSerializer
 
     def get(self, request, format=None):
         """ Retornar lista de caracteristicas del APIView """
@@ -17,4 +20,27 @@ class HelloApiView(APIView):
         ]
         return Response({'message': 'Hello', 'an_apiview': an_apiview})
 
+    def post(self, request):
+        """ Crea un mensaje con nuestro nombre """
+        serializer = self.serializer_class(data=request.data)
+        if serializer.is_valid():
+            name = serializer.validated_data.get('name')
+            message = f'Hello {name}'
+            return Response({'message': message})
+        else:
+            return Response(serializer.errors,
+                            status=status.HTTP_400_BAD_REQUEST
+                            )
+
+    def put(self, request, pk=None):
+        """Maneja actualizar un objeto"""
+        return Response({'method': 'PUT'})
+
+    def patch(self, request, pk=None):
+        """Maneja actualizar un objeto parcialmente"""
+        return Response({'method': 'PATCH'})
+
+    def delete(self, request, pk=None):
+        """Borra un objeto"""
+        return Response({'method': 'DELETE'})
 
